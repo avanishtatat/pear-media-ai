c753391a1f64de034c0787948edc5c62acb34ddf
avanishtatat <avanisht.at.at@gmail.com>
2026-03-29 15:41:26 +0530

    Fix: include actual backend and frontend files (remove nested git repos)

---
+++ frontend/public/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <main className='bg-[#AAABAC] w-screen h-screen'>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
